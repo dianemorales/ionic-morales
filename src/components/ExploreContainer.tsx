4611940f6803a4d00780c4e56afb230780e4b1f0
dianemorales <dianemorales330@gmail.com>
2024-03-07 15:22:19 +0800

    added card to components homepage

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,20 @@
 import './ExploreContainer.css';
+import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonCard>
+    <img alt="Silhouette of mountains" src="anime-cat-names-1204854078-2000-d34f509ae54943d8b78cfb4bf9ba0678.jpg" />
+    <IonCardHeader>
+      <IonCardTitle>Pussy Cat</IonCardTitle>
+      <IonCardSubtitle>Hello nebors</IonCardSubtitle>
+    </IonCardHeader>
+
+    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+  </IonCard>
   );
 };
 
